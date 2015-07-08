f1b4a43c8c5417724426081562303705a44832a0
Aimeow <wuzhaoyi@dudumeijia.com>
2015-07-08 15:47:20 +0800

    Initial Commit

---
+++ DDFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDFramework
+//
+//  Created by Aimeow on 7/8/15.
+//  Copyright (c) 2015 Aimeow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
