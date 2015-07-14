c5289f39f2012aa45a6b6c2f5e9aca8dcc9c5ae3
Aimeow <wuzhaoyi@dudumeijia.com>
2015-07-14 17:42:32 +0800

    double commit

--- DDFramework/AppDelegate.m
+++ DDFramework/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import "DDHttpManager.h"
+//#import "DDAlertView.h"
+#import "UMessage.h"
 
 @interface AppDelegate ()
 
@@ -16,30 +19,56 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+- (void)setUpUmengMessage:(NSDictionary*)launchOptions
+{
+    [UMessage startWithAppkey:kUmengMessageKey launchOptions:launchOptions];
+    //register remoteNotification types
+    //register remoteNotification types （iOS 8.0及其以上版本）
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)  {
+        UIMutableUserNotificationAction *action1 = [[UIMutableUserNotificationAction alloc] init];
+        action1.identifier = @"action1_identifier";
+        action1.title=@"Accept";
+        action1.activationMode = UIUserNotificationActivationModeForeground;//当点击的时候启动程序
+        
+        UIMutableUserNotificationAction *action2 = [[UIMutableUserNotificationAction alloc] init];  //第二按钮
+        action2.identifier = @"action2_identifier";
+        action2.title=@"Reject";
+        action2.activationMode = UIUserNotificationActivationModeBackground;//当点击的时候不启动程序，在后台处理
+        action2.authenticationRequired = YES;//需要解锁才能处理，如果action.activationMode = UIUserNotificationActivationModeForeground;则这个属性被忽略；
+        action2.destructive = YES;
+        //
+        UIMutableUserNotificationCategory *categorys = [[UIMutableUserNotificationCategory alloc] init];
+        categorys.identifier = @"category1";//这组动作的唯一标示
+        [categorys setActions:@[action1,action2] forContext:(UIUserNotificationActionContextDefault)];
+        
+        UIUserNotificationSettings *userSettings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge|UIUserNotificationTypeSound|UIUserNotificationTypeAlert categories:[NSSet setWithObject:categorys]];
+        [UMessage registerRemoteNotificationAndUserNotificationSettings:userSettings];
+    } else {
+        //register remoteNotification types (iOS 8.0以下)
+        #pragma clang diagnostic push
+        #pragma clang diagnostic ignored"-Wdeprecated-declarations"
+        [UMessage registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeSound|UIRemoteNotificationTypeAlert];
+        #pragma clang diagnostic pop
+    }
+    [UMessage setLogEnabled:NO];
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)setUpCocoaLumberjack
+{
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
+    [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor blueColor] backgroundColor:nil forFlag:DDLogFlagInfo];
+    [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor greenColor] backgroundColor:nil forFlag:DDLogFlagVerbose];
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
+- (void)setUpAppearance
+{
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
--- DDFramework/ViewController.m
+++ DDFramework/ViewController.m
@@ -7,21 +7,45 @@
 //
 
 #import "ViewController.h"
+#import "DDHttpManager.h"
+#import "DDBannerModel.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate >
+
+//@property (nonatomic , strong) UITableView *tableView;
+@property (nonatomic , strong) DDBannerModel *model;
+
 
 @end
 
 @implementation ViewController
+//- (IBAction)dotest:(id)sender {
+//    [self.model loadData];
+//}
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    
+    self.model = [DDBannerModel model];
+    self.model.isSubBanner = YES;
+    
+   [RACObserve(self.model, bannerArray) subscribeNext:^(NSMutableArray *array) {
+       NSLog(@"%@" , array);
+       ///NSObject调用print方法(NSObject+allProperties.h中的方法)可以遍历出所有的变量，可以在lldb中实用po调用(建议)，也可以在代码中调用（不建议），方便调试
+       for (DDBannerEntity *entity in array) {
+           [entity print];
+       }
+   }];
+    
+//    ///self.model.dataTask是系统中的NSURLSessionDataTask，可以做很多基础的操作
+//    ///可以停掉当前的请求
+//    [self.model.dataTask cancel];
+//    ///启动当前请求
+//    [self.model.dataTask resume];
+//
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 @end
